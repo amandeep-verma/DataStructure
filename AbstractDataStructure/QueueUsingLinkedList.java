dd78714822f82d15d28b4cec206a0007ebd49ac0
amandeep-verma <39786879+amandeep-verma@users.noreply.github.com>
2020-08-10 18:02:19 -0400

    updated implementation and queue program

--- AbstractDataStructure/QueueUsingLinkedList.java
+++ AbstractDataStructure/QueueUsingLinkedList.java
@@ -1,12 +1,14 @@
 package abstractDataType;
 
-// Stack implementation using Linked List
-public class StackUsingLinkedList<Item> {
+//Queue implementation Generic using Linked List
+public class QueueUsingLinkedList<Item> {
 	private Node first;
+	private Node last;
 	private int N;
 
-	StackUsingLinkedList() {
+	QueueUsingLinkedList() {
 		first = null;
+		last = null;
 		N = 0;
 	}
 
@@ -15,15 +17,21 @@ public class StackUsingLinkedList<Item> {
 		private Node next;
 	}
 
-	public void push(Item item) {
-		Node second = first;
-		first = new Node();
-		first.item = item;
-		first.next = second;
+	public void enqueue(Item item) {
+		
+		
+		Node newLast = new Node();
+		newLast.item = item;
+		newLast.next = null;
+		if(isEmpty())
+			first = newLast;
+		else
+			last.next = newLast;
+		last = newLast;
 		N++;
 	}
 
-	public Item pop() {
+	public Item dequeue() {
 		if (!isEmpty()) {
 			Item item = first.item;
 			first = first.next;
@@ -55,20 +63,21 @@ public class StackUsingLinkedList<Item> {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		StackUsingLinkedList<Integer> st = new StackUsingLinkedList<Integer>();
+		QueueUsingLinkedList<Integer> st = new QueueUsingLinkedList<Integer>();
 
-		System.out.println("pushing elements in the stack");
+		System.out.println("enqueueing elements in the stack");
 		for (int i = 0; i < 15; i++) {
-			st.push(i);
+			st.enqueue(i);
 		}
 
 		System.out.println("printing elements of the stack");
 		System.out.println(st);
 
-		System.out.println("popping elements out of the stack");
+		System.out.println("dequeueping elements out of the stack");
 		for (int i = 0; i < 15; i++) {
-			System.out.println("size= " + st.size() + " element= " + st.pop());
+			System.out.println("size= " + st.size() + " element= " + st.dequeue());
 		}
 
 	}
+
 }
